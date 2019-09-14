31c16d7af6c7d7b38d05fed5a351a4b5efd4c57c
uzinok <uzinok@yandex.ru>
2019-09-14 18:23:58 +0400

    подключил шрифты, верска header

--- gulp/copy.js
+++ gulp/copy.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 
 gulp.task("copy", function () {
   return gulp.src([
-    "src/fonts/**/*.{woff, woff2}*",
+    "src/fonts/*.{woff, woff2}*",
     "src/img/*.+(png|jpg|svg|webp|ico)*",
   ], {
       base: "src"
