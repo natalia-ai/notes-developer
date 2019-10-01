0cb62b940b967361b6ed3a13b273432078b3abd1
uzinok <uzinok@yandex.ru>
2019-10-01 20:57:39 +0400

    подготовка слайдера js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,35 @@
 function slider(my_slider) {
-  console.log(my_slider);
+
+  let arrSliderItem = my_slider.querySelectorAll("li"),
+      sliderList = my_slider.querySelector("ul");
+
+      console.log(arrSliderItem);
+      console.log(sliderList);
+
+  widhtSlider();
+  setWidthHeightSlider()
+
+  function widhtSlider() {
+    myWidhtSlider = my_slider.offsetWidth;
+    return myWidhtSlider;
+  }
+
+  function setWidthHeightSlider() {
+    sliderList.style.width = arrSliderItem.length * myWidhtSlider + "px";
+  }
+
+  window.addEventListener("resize", function () {
+    windowResize();
+  })
+
+  let check;
+
+  function windowResize() {
+    clearTimeout(check);
+    check = setTimeout(function () {
+      clearInterval(animations);
+      widhtSlider();
+      setWidthHeightSlider();
+    }, 100)
+  }
 }
