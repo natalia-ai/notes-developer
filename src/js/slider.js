bd70966527cf536927f53b6bcb399bf4bb65f83a
uzinok <uzinok@yandex.ru>
2019-10-02 17:36:06 +0400

    первое движение слайдера

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,7 @@
 function slider(my_slider) {
 
   let arrSliderItem = my_slider.querySelectorAll("li"),
-      sliderList = my_slider.querySelector("ul");
-
-      console.log(arrSliderItem);
-      console.log(sliderList);
+    sliderList = my_slider.querySelector("ul");
 
   widhtSlider();
   setWidthHeightSlider()
@@ -32,4 +29,49 @@ function slider(my_slider) {
       setWidthHeightSlider();
     }, 100)
   }
+
+  let 
+  checkMouseDown = false,
+  startClientX,
+  startMarginLeft;
+
+  sliderList.addEventListener("mousedown", function (e) {
+
+    startClientX = e.clientX;
+    startMarginLeft = Number(getComputedStyle(sliderList).marginLeft.replace("px", ""));
+    checkMouseDown = true;
+
+    console.log(startMarginLeft);
+
+    return startClientX, startMarginLeft, checkMouseDown;
+
+  })
+
+
+
+  sliderList.addEventListener("mousemove", function (e) {
+
+    if (checkMouseDown) {
+
+      
+      let newMarginLeft = startMarginLeft + -(startClientX - e.clientX)
+      
+      console.log(startClientX);
+
+      if (newMarginLeft > 0 ) {
+        newMarginLeft = 0;
+        startClientX = e.clientX;
+      } else if (newMarginLeft < -((arrSliderItem.length - 1) * myWidhtSlider)) {
+        newMarginLeft = -((arrSliderItem.length - 1) * myWidhtSlider)
+      }
+
+      sliderList.style.marginLeft = newMarginLeft + "px";
+    }
+
+  })
+
+  document.addEventListener("mouseup", function (e) {
+    checkMouseDown = false;
+  })
+
 }
